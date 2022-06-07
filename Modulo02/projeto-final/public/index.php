b5f5b3d8dd84ec82b4748b53258d4d6267288fd1
= <bethaniafreitas@yahoo.com.br>
2022-06-07 15:57:54 -0300

    projeto concluido

--- Modulo02/projeto-final/public/index.php
+++ Modulo02/projeto-final/public/index.php
@@ -9,17 +9,30 @@ use App\Controller\ProductController;
 
 $url = explode('?' , $_SERVER['REQUEST_URI'])[0];
 
-if ($url === '/'){
-    $c = new IndexController();
-    $c->indexAction();
-} elseif($url === '/login') {
-    $c = new IndexController();
-    $c->loginAction();
-} elseif($url ==='/produtos'){
-    $p = new ProductController();
-    $p->listAction();
-} else {
-    $e = new ErrorController();
-    $e->notFoundAction();
+function createRoute(string $controllerName, string $methodName){
+    return [
+        'controller'=> $controllerName,
+        'method' => $methodName,
+
+    ];
+
 }
+
+$routes = [
+    '/'=> createRoute(IndexController::class, 'indexAction'),
+    '/produtos' => createRoute(ProductController::class, 'listAction'),
+    '/produtos/novo' => createRoute(ProductController::class, 'addAction'),
+
+
+];
+
+if (false ===isset($routes[$url])){
+    (new ErrorController())->notFoundAction();
+    exit;
+}
+
+$controllerName = $routes[$url] ['controller'];
+$methodName = $routes[$url] ['method'];
+
+(new $controllerName())-> $methodName();
 
